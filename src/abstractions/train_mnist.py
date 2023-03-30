ccba87efd999950690f62a3eac973ad1f84c5e76
Erik Jenner <erik.jenner99@gmail.com>
2023-03-30 16:04:21 -0700

    Update MNIST training to use refactored code

--- src/abstractions/train_mnist.py
+++ src/abstractions/train_mnist.py
@@ -2,175 +2,168 @@
 
 from collections import defaultdict
 import sys
+import hydra
 import jax
 import jax.numpy as jnp
 import numpy as np
 from flax import linen as nn
+from jax.config import config as jax_config
 
 from flax.training import train_state
+from omegaconf import DictConfig, OmegaConf
 import optax
 from loguru import logger
 import argparse
 from clearml import Task
 
-from abstractions import data, utils
+from abstractions import data, trainer, utils
+from abstractions.logger import DummyLogger, WandbLogger
 
 
 class MLP(nn.Module):
     """A simple feed-forward MLP."""
 
+    hidden_dim: int = 256
+    output_dim: int = 10
+
     @nn.compact
-    def __call__(self, x, return_activations=False):
+    def __call__(self, x, return_activations=False, train=True):
         activations = []
         x = x.reshape((x.shape[0], -1))  # flatten
-        x = nn.Dense(features=256)(x)
+        x = nn.Dense(features=self.hidden_dim)(x)
         x = nn.relu(x)
         activations.append(x)
-        x = nn.Dense(features=256)(x)
+        x = nn.Dense(features=self.hidden_dim)(x)
         x = nn.relu(x)
         activations.append(x)
-        x = nn.Dense(features=10)(x)
+        x = nn.Dense(features=self.output_dim)(x)
 
         if return_activations:
             return x, activations
         return x
 
 
-@jax.jit
-def apply_model(state, batch):
-    """Computes gradients, loss and metrics for a single batch."""
+class MNISTTrainer(trainer.TrainerModule):
+    def __init__(self, hidden_dim: int, output_dim: int, **kwargs):
+        super().__init__(
+            model_class=MLP,  # type: ignore
+            model_hparams={"hidden_dim": hidden_dim, "output_dim": output_dim},
+            **kwargs,
+        )
 
-    images, labels, backdoored = batch
+    def create_functions(self):
+        def losses(params, state, batch):
+            images, labels, infos = batch
+            logits = state.apply_fn({"params": params}, images)
+            one_hot = jax.nn.one_hot(labels, 10)
+            loss = jnp.mean(optax.softmax_cross_entropy(logits=logits, labels=one_hot))
+            correct = jnp.argmax(logits, -1) == labels
+            accuracy = jnp.mean(correct)
+            return loss, accuracy
 
-    def loss_fn(params):
-        logits = state.apply_fn({"params": params}, images)
-        one_hot = jax.nn.one_hot(labels, 10)
-        loss = jnp.mean(optax.softmax_cross_entropy(logits=logits, labels=one_hot))
-        return loss, logits
-
-    grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
-    (loss, logits), grads = grad_fn(state.params)
-
-    correct = jnp.argmax(logits, -1) == labels
-    num_backdoored = jnp.sum(backdoored)
-    # Using this slightly convoluted way of computing accuracy on subsets
-    # to make everything jit-able.
-    backdoor_accuracy = jnp.sum(correct * backdoored) / num_backdoored
-    non_backdoor_accuracy = jnp.sum(correct * (1 - backdoored)) / (
-        len(backdoored) - num_backdoored
-    )
-    accuracy = jnp.mean(correct)
-    metrics = {
-        "loss": loss,
-        "accuracy": accuracy,
-        "backdoor_accuracy": backdoor_accuracy,
-        "non_backdoor_accuracy": non_backdoor_accuracy,
-    }
-    return grads, metrics
-
-
-@jax.jit
-def update_model(state, grads):
-    return state.apply_gradients(grads=grads)
-
-
-def train_epoch(state, train_loader, rng, metrics_logger):
-    """Train for a single epoch."""
-    train_ds_size = len(train_loader.dataset)
-    steps_per_epoch = train_ds_size // train_loader.batch_size
-
-    epoch_metrics = defaultdict(list)
-
-    for batch in train_loader:
-        grads, metrics = apply_model(state, batch)
-        state = update_model(state, grads)
-        for k, v in metrics.items():
-            epoch_metrics[k].append(v)
-            metrics_logger.report_scalar(
-                title="Training", series=k, value=v.item(), iteration=int(state.step)
+        def train_step(state, batch):
+            loss_fn = lambda params: losses(params, state, batch)
+            (loss, accuracy), grads = jax.value_and_grad(loss_fn, has_aux=True)(
+                state.params
             )
 
-    train_metrics = {k: np.mean(v) for k, v in epoch_metrics.items()}
-    return state, train_metrics
+            param_norms = jax.tree_map(lambda x: jnp.linalg.norm(x), state.params)
+            max_param_norm = jnp.max(jnp.array(jax.tree_util.tree_leaves(param_norms)))
+
+            grad_norms = jax.tree_map(lambda x: jnp.linalg.norm(x), grads)
+            max_grad_norm = jnp.max(jnp.array(jax.tree_util.tree_leaves(grad_norms)))
+
+            state = state.apply_gradients(grads=grads)
+            metrics = {
+                "loss": loss,
+                "accuracy": accuracy,
+                "max_param_norm": max_param_norm,
+                "max_grad_norm": max_grad_norm,
+            }
+            return state, metrics
+
+        def eval_step(state, batch):
+            loss, accuracy = losses(state.params, state, batch)
+            metrics = {
+                "loss": loss,
+                "accuracy": accuracy,
+            }
+            return metrics
+
+        return train_step, eval_step
+
+    def on_training_epoch_end(self, epoch_idx, metrics):
+        logger.log("METRICS", self._prettify_metrics(metrics))
+
+    def on_validation_epoch_end(self, epoch_idx: int, metrics, val_loader):
+        logger.log("METRICS", self._prettify_metrics(metrics))
+
+    def _prettify_metrics(self, metrics):
+        return "\n".join(f"{k}: {v:.4f}" for k, v in metrics.items())
 
 
-def create_train_state(rng, config):
-    """Creates initial `TrainState`."""
-    model = MLP()
-    params = model.init(rng, jnp.ones([1, 28, 28, 1]))["params"]
-    tx = optax.sgd(config.learning_rate, config.momentum)
-    return train_state.TrainState.create(apply_fn=model.apply, params=params, tx=tx)
-
-
-def train_and_evaluate(config) -> train_state.TrainState:
+@hydra.main(version_base=None, config_path="conf", config_name="mnist")
+def train_and_evaluate(cfg: DictConfig):
     """Execute model training and evaluation loop.
 
     Args:
-      config: Hyperparameter configuration for training and evaluation.
-
-    Returns:
-      The train state (which includes the `.params`).
+      cfg: Hydra configuration object.
     """
-    # seeds pytorch and numpy
-    Task.set_random_seed(0)
-    task = Task.init(
-        project_name="backdoor-detection", task_name="train MNIST backdoor"
-    )
-    metrics_logger = task.get_logger()
+    if cfg.debug:
+        jax_config.update("jax_debug_nans", True)
+        jax_config.update("jax_disable_jit", True)
+        cfg.wandb = False
 
-    train_loader, test_loader = data.get_data_loaders(config.batch_size)
-    rng = jax.random.PRNGKey(0)
-
-    rng, init_rng = jax.random.split(rng)
-    state = create_train_state(init_rng, config)
-
-    for epoch in range(1, config.num_epochs + 1):
-        rng, input_rng = jax.random.split(rng)
-        metrics_logger.report_scalar("epoch", "epoch", epoch, int(state.step))
-        state, train_metrics = train_epoch(
-            state, train_loader, input_rng, metrics_logger
+    if cfg.wandb:
+        metrics_logger = WandbLogger(
+            project_name="abstractions",
+            tags=["mnist-backdoor-training"],
+            config=OmegaConf.to_container(cfg, resolve=True),  # type: ignore
         )
-        test_batch = next(iter(test_loader))
-        _, test_metrics = apply_model(state, test_batch)
-
-        logger.log(
-            "METRICS",
-            "epoch:% 3d, train_loss: %.4f, train_accuracy: %.2f, test_loss: %.4f, test_accuracy: %.2f"
-            % (
-                epoch,
-                train_metrics["loss"],
-                train_metrics["accuracy"] * 100,
-                test_metrics["loss"],
-                test_metrics["accuracy"] * 100,
-            ),
-        )
-
-    if config.save_path:
-        utils.save(state.params, config.save_path)
     else:
-        utils.save(state.params, "models/mnist", overwrite=True)
+        metrics_logger = DummyLogger()
 
-    return state
-
-
-def parse_args():
-    parser = argparse.ArgumentParser(description="Jax MNIST training example")
-    parser.add_argument(
-        "--num_epochs", type=int, default=10, help="Number of epochs to train"
+    train_loader = data.get_data_loaders(
+        cfg.batch_size,
+        transforms=data.get_transforms(backdoor_options={"p_backdoor": 0.5}),
+        train=True,
     )
-    parser.add_argument("--batch_size", type=int, default=128, help="Batch size")
-    parser.add_argument(
-        "--learning_rate", type=float, default=0.1, help="Learning rate"
+    val_loader = data.get_data_loaders(
+        cfg.batch_size,
+        transforms=data.get_transforms(backdoor_options={"p_backdoor": 0.5}),
+        train=False,
     )
-    parser.add_argument("--momentum", type=float, default=0.9, help="Momentum")
-    parser.add_argument("--save_path", type=str, default=None)
-    parser.add_argument(
-        "--workdir", type=str, default="logs", help="Directory for logs"
+    val_loaders = {
+        "val": val_loader,
+    }
+
+    # Dataloader returns logits and activations, only activations get passed to model
+    images, _, _ = next(iter(train_loader))
+    example_input = images[0:1]
+
+    trainer = MNISTTrainer(
+        hidden_dim=cfg.hidden_dim,
+        output_dim=10,
+        optimizer=hydra.utils.instantiate(cfg.optim),
+        example_input=example_input,
+        # Hydra sets the cwd to the right log dir automatically
+        log_dir=".",
+        check_val_every_n_epoch=1,
+        loggers=[metrics_logger],
+        enable_progress_bar=False,
     )
-    return parser.parse_args()
+
+    trainer.train_model(
+        train_loader=train_loader,
+        val_loaders=val_loaders,
+        test_loaders=None,
+        num_epochs=cfg.num_epochs,
+    )
+    trainer.save_model()
+    trainer.close_loggers()
 
 
-def main():
+if __name__ == "__main__":
     logger.remove()
     logger.level("METRICS", no=25, color="<green>", icon="📈")
     logger.add(
@@ -178,9 +171,4 @@ def main():
     )
     # Default logger for everything else:
     logger.add(sys.stderr, filter=lambda record: record["level"].name != "METRICS")
-    config = parse_args()
-    train_and_evaluate(config)
-
-
-if __name__ == "__main__":
-    main()
+    train_and_evaluate()
