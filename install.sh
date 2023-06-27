d5b1c9adf244482ecfc46855edba57b0cf639f50
Erik Jenner <erik.jenner99@gmail.com>
2023-06-27 21:28:25 +0000

    Fix installation script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip3 install -r torch-requirements.txt
-pip3 install -r cuda-requirements.txt
 pip3 install -r requirements.txt
+pip3 install -r cuda-requirements.txt
 pip3 install -e .
