ddc504afbb5e2fba03a8f0a18ca5c5d1c3d3a262
Erik Jenner <erik.jenner99@gmail.com>
2023-10-20 14:33:09 -0700

    Fix validation set configuration via CLI

--- src/cupbearer/scripts/conf/train_classifier_conf.py
+++ src/cupbearer/scripts/conf/train_classifier_conf.py
@@ -1,13 +1,42 @@
+import dataclasses
 import os
 from dataclasses import dataclass
 from typing import Optional
 
-from cupbearer.data import DatasetConfig
+from cupbearer.data import DatasetConfig, NoData
 from cupbearer.models import CNN, MLP, ModelConfig
-from cupbearer.utils.config_groups import config_group
+from cupbearer.utils.config_groups import (
+    config_group,
+    register_config_group,
+)
 from cupbearer.utils.optimizers import Adam, OptimizerConfig
 from cupbearer.utils.scripts import DirConfig, ScriptConfig
-from simple_parsing.helpers import dict_field, mutable_field
+from cupbearer.utils.utils import BaseConfig
+from simple_parsing.helpers import mutable_field
+
+
+@dataclass(kw_only=True)
+class ValidationConfig(BaseConfig):
+    val: Optional[DatasetConfig] = config_group(DatasetConfig, NoData)
+    clean: Optional[DatasetConfig] = config_group(DatasetConfig, NoData)
+    backdoor: Optional[DatasetConfig] = config_group(DatasetConfig, NoData)
+
+    def items(self) -> list[tuple[str, DatasetConfig]]:
+        res = []
+        for field in dataclasses.fields(self):
+            value = getattr(self, field.name)
+            if isinstance(value, DatasetConfig) and not isinstance(value, NoData):
+                res.append((field.name, value))
+
+        return res
+
+
+register_config_group(
+    ValidationConfig,
+    {
+        "default": ValidationConfig,
+    },
+)
 
 
 @dataclass(kw_only=True)
@@ -15,7 +44,7 @@ class Config(ScriptConfig):
     model: ModelConfig = config_group(ModelConfig)
     train_data: DatasetConfig = config_group(DatasetConfig)
     optim: OptimizerConfig = config_group(OptimizerConfig, Adam)
-    val_data: dict[str, DatasetConfig] = dict_field()
+    val_data: ValidationConfig = config_group(ValidationConfig, ValidationConfig)
     num_epochs: int = 10
     batch_size: int = 128
     max_batch_size: int = 2048
