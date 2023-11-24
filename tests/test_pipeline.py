442cb21c162beb1aa5ca29d78e2ef54492f2ea31
Viktor Rehnberg <viktor.rehnberg@gmail.com>
2023-11-24 12:02:45 +0100

    Fix filter_advex_failure

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -90,11 +90,11 @@ def test_train_autoencoder_corner_backdoor(backdoor_classifier_path, tmp_path):
     assert (tmp_path / "eval.json").is_file()
 
 
-def filter_advex_failure(err, *args):
+def filter_advex_failure(exc_info, *args):
     flaky_error_msg = (
         "RuntimeError(\nRuntimeError: Attack failed, new accuracy is 100.0 > 1.0.\n"
     )
-    return err[1].stderr.endswith(flaky_error_msg)
+    return exc_info[1].stderr.decode().endswith(flaky_error_msg)
 
 
 @pytest.mark.flaky(max_runs=3, rerun_filter=filter_advex_failure)
