c520d684e781f2aa45aa5e025d5940c41bca9e6d
Erik Jenner <erik.jenner99@gmail.com>
2024-07-04 22:45:58 -0700

    More simple extractor tests

--- tests/test_extractors.py
+++ tests/test_extractors.py
@@ -1,5 +1,6 @@
 import pytest
 import torch
+from cupbearer import models
 from cupbearer.detectors.extractors.activation_extractor import ActivationExtractor
 from cupbearer.detectors.extractors.core import IdentityExtractor
 
@@ -11,8 +12,10 @@ class DummyModel(torch.nn.Module):
         # placement.
         self.dummy_param = torch.nn.Parameter(torch.randn(1))
         self.dummy_module = torch.nn.Identity()
+        self.called = False
 
     def forward(self, x):
+        self.called = True
         return self.dummy_module(x)
 
 
@@ -22,8 +25,17 @@ def dummy_model():
 
 
 @pytest.fixture
+def mlp():
+    return models.MLP(
+        input_shape=(3, 8, 8),
+        output_dim=10,
+        hidden_dims=[4, 6],
+    )
+
+
+@pytest.fixture
 def dummy_batch():
-    return torch.randn(10, 3, 28, 28)
+    return torch.randn(10, 3, 8, 8)
 
 
 EXTRACTOR_FACTORIES = [
@@ -43,3 +55,32 @@ def test_extractor_basics(ExtractorFactory, dummy_model, dummy_batch):
     assert extractor.model is dummy_model
     result = extractor(dummy_batch)
     assert result is not None
+
+
+def test_activation_extractor_dummy(dummy_model, dummy_batch):
+    extractor = ActivationExtractor(names=["dummy_module.output"])
+    extractor.set_model(dummy_model)
+    acts = extractor(dummy_batch)
+    assert dummy_model.called
+    assert len(acts) == 1
+    assert torch.allclose(acts["dummy_module.output"], dummy_batch)
+
+
+def test_activation_extractor(mlp, dummy_batch):
+    names = (
+        [f"layers.linear_{i}.output" for i in range(2)]
+        + [f"layers.relu_{i}.output" for i in range(2)]
+        + [f"layers.linear_{i}.input" for i in range(2)]
+        + [f"layers.relu_{i}.input" for i in range(2)]
+    )
+    extractor = ActivationExtractor(names=names)
+    extractor.set_model(mlp)
+    acts = extractor(dummy_batch)
+    assert set(acts.keys()) == set(names)
+    assert acts["layers.linear_0.output"].shape == (10, 4)
+    assert acts["layers.linear_1.output"].shape == (10, 6)
+    assert acts["layers.relu_0.output"].shape == (10, 4)
+    assert acts["layers.relu_1.output"].shape == (10, 6)
+    assert torch.allclose(acts["layers.linear_0.input"], dummy_batch.view(10, -1))
+    assert torch.allclose(acts["layers.linear_1.input"], acts["layers.relu_0.output"])
+    assert torch.allclose(acts["layers.relu_1.input"], acts["layers.linear_1.output"])
