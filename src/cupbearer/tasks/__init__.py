3a7298a8f335d7a52442d32d23d1c2bf6960d532
Erik Jenner <erik.jenner99@gmail.com>
2024-07-30 16:18:58 -0700

    Add HF task

--- src/cupbearer/tasks/__init__.py
+++ src/cupbearer/tasks/__init__.py
@@ -1,6 +1,7 @@
 # ruff: noqa: F401
 from .adversarial_examples import adversarial_examples
 from .backdoor_detection import backdoor_detection
+from .huggingface_task import hf_task
 from .measurement_tampering import measurement_tampering
 from .quirky_lm import quirky_lm
 from .task import Task
